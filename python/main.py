8959e8eecc39c7accc9c0375b91f7595c358e267
hulu <754341768@qq.com>
2024-05-26 15:17:29 +0800

    init

--- python/main.py
+++ python/main.py
@@ -13,30 +13,38 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-ignored_model_names = ["gpt-4", "gpt-3.5", "websearch", "dall-e-3", "gpt-4o"]
-image_model = ["dalle3", "dalle-3", "dall-e-3"]
 
-logging.basicConfig(level=logging.INFO)
-# 存储map，包含channel_id和到期时间
+# Constants
+IGNORED_MODEL_NAMES = ["gpt-4", "gpt-3.5", "websearch", "dall-e-3", "gpt-4o"]
+IMAGE_MODEL_NAMES = ["dalle3", "dalle-3", "dall-e-3"]
+AUTH_TOKEN = os.getenv("AUTHORIZATION")
+
+# Storage map containing channel_id and expiration time
 storage_map = {}
 
+# Logging configuration
+logging.basicConfig(level=logging.INFO)
 
-def process_content(msg):
+
+def process_content(message):
     text_array = []
     image_url_array = []
-    if isinstance(msg, str):
-        return msg, image_url_array
-    elif isinstance(msg, list):
-        for message in msg:
-            content_type = message.get("type")
+
+    if isinstance(message, str):
+        return message, image_url_array
+
+    if isinstance(message, list):
+        for msg in message:
+            content_type = msg.get("type")
             if content_type == "text":
-                text_array.append(message.get("text", ""))
+                text_array.append(msg.get("text", ""))
             elif content_type == "image_url":
-                url = message.get("image_url", {}).get("url", "")
+                url = msg.get("image_url", {}).get("url", "")
                 if is_base64_image(url):
                     url = upload_image_to_telegraph(url)
                 image_url_array.append(url)
-        return '\n'.join(text_array), image_url_array
+
+    return '\n'.join(text_array), image_url_array
 
 
 def upload_image_to_telegraph(base64_string):
@@ -46,23 +54,16 @@ def upload_image_to_telegraph(base64_string):
         image_data = base64.b64decode(base64_string)
 
         image_type = imghdr.what(None, image_data)
-
         if image_type is None:
             raise ValueError("Invalid image data")
 
         mime_type = f"image/{image_type}"
-        files = {
-            'file': (f'image.{image_type}', image_data, mime_type)
-        }
+        files = {'file': (f'image.{image_type}', image_data, mime_type)}
         response = requests.post('https://telegra.ph/upload', files=files)
 
-        # 检查响应状态代码
-        response.raise_for_status()  # 如果状态码不是200，会引发HTTPError
-
-        # 解析响应的JSON数据
+        response.raise_for_status()
         json_response = response.json()
         if isinstance(json_response, list) and 'src' in json_response[0]:
-            # 返回图片的URL
             return 'https://telegra.ph' + json_response[0]['src']
         else:
             raise ValueError(f"Unexpected response format: {json_response}")
@@ -77,14 +78,13 @@ def is_base64_image(base64_string):
     return base64_string.startswith('data:image')
 
 
-def get_user_contents(messages, final_user_content, limit=3):
+def get_user_contents(messages, limit=3):
     contents = []
     user_content_added = False
     for message in messages:
-        if message.get("role") == "user":
-            if not user_content_added:
-                contents.append(str(message.get("content", '')))
-                user_content_added = True
+        if message.get("role") == "user" and not user_content_added:
+            contents.append(str(message.get("content", '')))
+            user_content_added = True
     return contents
 
 
@@ -94,16 +94,12 @@ def generate_hash(contents, model_name):
 
 
 def get_channel_id(hash_value, token, model_name, content, template_id):
-    # 检查缓存中是否存在且未过期
     if hash_value in storage_map:
         channel_id, expiry_time = storage_map[hash_value]
         if expiry_time > datetime.now() and channel_id:
             return channel_id
 
-    # 如果缓存不存在或已过期，调用第三方接口获取新的 channel_id
     channel_id = fetch_channel_id(token, model_name, content, template_id)
-
-    # 将新的 channel_id 存储到缓存中，有效期设置为1天
     expiry_time = datetime.now() + timedelta(days=1)
     storage_map[hash_value] = (channel_id, expiry_time)
 
@@ -142,29 +138,83 @@ def fetch_channel_id(auth_token, model_name, content, template_id):
 
     start_time = time.time()
     try:
-        resp = requests.post(url, headers=headers, json=data)
-        # logging.info("fetch_channel_id resp: %s", resp.json())
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        logging.info("fetch_channel_id elapsed_time: %.2f seconds", elapsed_time)
-        # logging.info("get channel id: %s", resp.json())
-
-        resp.raise_for_status()
-        response_data = resp.json()
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        response_data = response.json()
         return response_data.get('data', {}).get('channelId')
 
     except requests.exceptions.RequestException as e:
-        end_time = time.time()
-        logging.error("fetch_channel_id error: %s, elapsed_time: %.2f seconds", e, end_time - start_time)
+        logging.error("fetch_channel_id error: %s", e)
         raise Exception(f"Failed to fetch channel_id. Error: {e}")
+    finally:
+        elapsed_time = time.time() - start_time
+        logging.info("fetch_channel_id elapsed_time: %.2f seconds", elapsed_time)
 
-    except Exception as e:
-        logging.error("An unexpected error occurred: %s", e)
-        raise
+
+def stream_2_json(resp, model_name):
+    logging.info("Entering stream_2_json function")
+
+    chunks = []
+    buffer = ""
+    json_object_counter = 0
+    merged_content = ""  # Variable to store the merged content
+    append_to_chunks = chunks.append  # Store the append function as a local variable
+    decode_utf8 = bytes.decode  # Store the decode function as a local variable
+    loads = json.loads  # Store the loads function as a local variable
+    for chunk in resp.iter_content(chunk_size=None):
+        buffer += decode_utf8(chunk, 'utf-8')
+        while "\n\n" in buffer:
+            json_object, buffer = buffer.split("\n\n", 1)
+            if json_object.startswith("data:"):
+                json_object = json_object[5:].strip()
+                json_object_counter += 1
+                if json_object_counter == 1:
+                    continue
+                try:
+                    chunk_json = loads(json_object)
+                except json.JSONDecodeError as e:
+                    logging.error(f"Failed to parse JSON: {e}")
+                    continue
+                for message in chunk_json:
+                    message_id = message.get("messageId", "")
+                    objectid = message.get("chunkId", "")
+                    content = message.get("content", "")
+                    merged_content += content
+
+                wrapped_chunk = {
+                    "id": message_id,
+                    "object": objectid,
+                    "created": 0,
+                    "model": model_name,
+                    "choices": [
+                        {
+                            "index": 0,
+                            "delta": {
+                                "role": "assistant",
+                                "content": merged_content
+                            },
+                            "finish_reason": "stop",
+                        }
+                    ],
+                    "usage": {
+                        "prompt_tokens": 0,
+                        "completion_tokens": 0,
+                        "total_tokens": 0
+                    },
+                    "system_fingerprint": None
+                }
+                append_to_chunks(wrapped_chunk)
+
+    logging.info("Exiting stream_2_json function")
+    return jsonify(chunks[-1])
 
 
 def send_chat_message(req, auth_token, channel_id, final_user_content, model_name, stream, image_url):
+    logging.info("Channel ID: %s", channel_id)
+    # logging.info("Final User Content: %s", final_user_content)
+    logging.info("Model Name: %s", model_name)
+    logging.info("Image URL: %s", image_url)
+    logging.info("Stream: %s", stream)
     url = "https://api.popai.pro/api/v1/chat/send"
     headers = {
         "Accept": "text/event-stream",
@@ -210,23 +260,19 @@ def send_chat_message(req, auth_token, channel_id, final_user_content, model_nam
 
     start_time = time.time()
     try:
-        # logging.info("post url= %s headers = %s data = %s", url, headers, data)
-        resp = requests.post(url, headers=headers, json=data)
-        # logging.info("send_chat_message resp: %s", resp)
-        end_time = time.time()
-        logging.info("send_chat_message elapsed_time: %.2f seconds", end_time - start_time)
+        response = requests.post(url, headers=headers, json=data)
+        if stream:
+            return stream_response(req, response, model_name)
+        else:
+            return stream_2_json(response, model_name)
+        # return jsonify(response.json())
+
     except requests.exceptions.RequestException as e:
-        end_time = time.time()
-        # 处理异常，例如打印错误信息
-        logging.error("requests error occurred: %s; elapsed_time: %.2f seconds ", e, end_time - start_time)
+        logging.error("send_chat_message error: %s", e)
         return Response(status=500)
-
-    if resp.status_code != 200:
-        return Response(status=resp.status_code)
-
-    if stream:
-        return stream_response(req, resp, model_name)
-    return jsonify(resp.json())
+    finally:
+        elapsed_time = time.time() - start_time
+        logging.info("send_chat_message elapsed_time: %.2f seconds", elapsed_time)
 
 
 def map_model_name(model_name):
@@ -240,55 +286,40 @@ def map_model_name(model_name):
         "internet": "Web Search",
         "gpt-4o": "GPT-4o"
     }
-    # 按键的长度从长到短排序
     sorted_keys = sorted(model_mapping.keys(), key=len, reverse=True)
-
     for key in sorted_keys:
         if model_name.lower().startswith(key):
             return model_mapping[key]
-
     return "GPT-4"
 
 
 def fetch(req):
-    # logging.info("body %s", req.json)
     if req.method == "OPTIONS":
         return Response(status=204, headers={'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'})
-    final_user_content, last_system_content, channel_id = None, None, None
-    image_url = []
+
     body = req.get_json()
     messages = body.get("messages", [])
     model_name = body.get("model")
     prompt = body.get("prompt", False)
     stream = body.get("stream", True)
-    auth_token = os.getenv("AUTHORIZATION")
+
     model_to_use = map_model_name(model_name)
-    template_id = 2000000 if model_name in image_model else ''
+    template_id = 2000000 if model_name in IMAGE_MODEL_NAMES else ''
 
     if not messages and prompt:
-        content = prompt
         final_user_content = prompt
         channel_id = os.getenv("CHAT_CHANNEL_ID")
     elif messages:
         last_message = messages[-1]
         final_user_content, image_url = process_content(last_message.get('content'))
-
-        # 获取前三条role为assistant的content值，并生成hash
-        user_contents = get_user_contents(messages, final_user_content)
+        user_contents = get_user_contents(messages)
         hash_value = generate_hash(user_contents, model_to_use)
-
-        # 获取channel_id
-        channel_id = get_channel_id(hash_value, auth_token, model_to_use, final_user_content, template_id)
-
-    logging.info("image_url %s", image_url)
+        channel_id = get_channel_id(hash_value, AUTH_TOKEN, model_to_use, final_user_content, template_id)
 
     if final_user_content is None:
         return Response("No user message found", status=400)
 
-    logging.info("channelId %s", channel_id)
-    logging.info("model_name %s", model_to_use)
-    logging.info("template_id %s", template_id)
-    return send_chat_message(req, auth_token, channel_id, final_user_content, model_to_use, stream, image_url)
+    return send_chat_message(req, AUTH_TOKEN, channel_id, final_user_content, model_to_use, stream, image_url)
 
 
 def stream_response(req, resp, model_name):
@@ -363,7 +394,7 @@ def list_models():
             "object": "model",
             "created": int(time.time()),
             "owned_by": "popai"
-        } for m in ignored_model_names]
+        } for m in IGNORED_MODEL_NAMES]
     }
 
 
