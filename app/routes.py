4af717442ab1b0ab938b86d62471877282a170ae
hulu <754341768@qq.com>
2024-06-06 10:42:21 +0800

    feat: add file upload

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,12 @@
 import logging
-from datetime import datetime, timedelta
 
-from flask import request, Response, current_app as app
+from flask import request, current_app as app
 
-from app.config import IGNORED_MODEL_NAMES, IMAGE_MODEL_NAMES, AUTH_TOKEN, HISTORY_MSG_LIMIT
+from app.config import IMAGE_MODEL_NAMES
 from app.config import configure_logging
-from app.utils import send_chat_message, fetch_channel_id, map_model_name, process_content, get_user_contents, \
-    generate_hash, get_next_auth_token, handle_error, get_request_parameters
+from app.utils import handle_error, fetch, generate_model_data, upload_file
 
 configure_logging()
-storage_map = {}
 
 
 @app.route("/v1/chat/completions", methods=["GET", "POST", "OPTIONS"])
@@ -23,18 +20,10 @@ def onRequest():
 
 @app.route('/v1/models')
 def list_models():
-    return {
-        "object": "list",
-        "data": [{
-            "id": m,
-            "object": "model",
-            "created": int(datetime.now().timestamp()),
-            "owned_by": "popai"
-        } for m in IGNORED_MODEL_NAMES]
-    }
+    return generate_model_data()
 
 
-@app.route('/v1/images/generations', methods= ["post"])
+@app.route('/v1/images/generations', methods=["post"])
 def image():
     try:
         request.get_json()["model"] = IMAGE_MODEL_NAMES[0]
@@ -42,47 +31,3 @@ def image():
     except Exception as e:
         logging.error("An error occurred with image : %s", e)
         return handle_error(e)
-
-
-def get_channel_id(hash_value, token, model_name, content, template_id):
-    if hash_value in storage_map:
-        channel_id, expiry_time = storage_map[hash_value]
-        if expiry_time > datetime.now() and channel_id:
-            logging.info("Returning channel id from cache")
-            return channel_id
-    channel_id = fetch_channel_id(token, model_name, content, template_id)
-    expiry_time = datetime.now() + timedelta(days=1)
-    storage_map[hash_value] = (channel_id, expiry_time)
-    return channel_id
-
-
-def fetch(req):
-    if req.method == "OPTIONS":
-        return handle_options_request()
-    token = get_next_auth_token(AUTH_TOKEN)
-    messages, model_name, prompt, user_stream = get_request_parameters(req.get_json())
-    model_to_use = map_model_name(model_name)
-    template_id = 2000000 if model_name in IMAGE_MODEL_NAMES else ''
-
-    if not messages and prompt:
-        final_user_content = prompt
-        first_user_message = final_user_content
-        image_url = None
-    elif messages:
-        last_message = messages[-1]
-        first_user_message, end_user_message, concatenated_messages = get_user_contents(messages, HISTORY_MSG_LIMIT)
-        final_user_content, image_url = process_content(last_message.get('content'))
-        final_user_content = concatenated_messages + '\n' + final_user_content if concatenated_messages else final_user_content
-        # channel_id = get_channel_id(hash_value, token, model_to_use, final_user_content, template_id)
-
-    hash_value = generate_hash(first_user_message, model_to_use, token)
-    channel_id = get_channel_id(hash_value, token, model_to_use, final_user_content, template_id)
-
-    if final_user_content is None:
-        return Response("No user message found", status=400)
-
-    return send_chat_message(req, token, channel_id, final_user_content, model_to_use, user_stream, image_url, model_name)
-
-
-def handle_options_request():
-    return Response(status=204, headers={'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'})
--- app/utils.py
+++ app/utils.py
@@ -5,17 +5,22 @@ import json
 import logging
 import os
 import re
+import tempfile
 from collections import deque
+from datetime import datetime, timedelta
+from urllib.parse import urlparse
 
 import requests
 from flask import Response, jsonify
 from requests.exceptions import ProxyError
 
+from app.config import IGNORED_MODEL_NAMES, AUTH_TOKEN, HISTORY_MSG_LIMIT
 from app.config import configure_logging, IMAGE_MODEL_NAMES, ProxyPool
 
 configure_logging()
 proxy_pool = ProxyPool()
 current_token_index = 0
+storage_map = {}
 
 
 def send_http_request(url, headers, data):
@@ -200,7 +205,7 @@ def process_content(message):
                 text_array.append(msg.get("text", ""))
             elif content_type == "image_url":
                 url = msg.get("image_url", {}).get("url", "")
-                if is_base64_image(url):
+                if is_base64(url):
                     url = upload_image_to_telegraph(url)
                 image_url_array.append(url)
 
@@ -432,3 +437,233 @@ def request_with_proxy(url, headers, data, stream, files):
         logging.error(f"Proxy error occurred: {e}")
         raise Exception("Proxy error occurred")
     return response
+
+
+def get_channel_id(hash_value, token, model_name, content, template_id):
+    if hash_value in storage_map:
+        channel_id, expiry_time = storage_map[hash_value]
+        if expiry_time > datetime.now() and channel_id:
+            logging.info("Returning channel id from cache")
+            return channel_id
+    channel_id = fetch_channel_id(token, model_name, content, template_id)
+    expiry_time = datetime.now() + timedelta(days=1)
+    storage_map[hash_value] = (channel_id, expiry_time)
+    return channel_id
+
+
+def fetch(req):
+    if req.method == "OPTIONS":
+        return handle_options_request()
+    token = get_next_auth_token(AUTH_TOKEN)
+    messages, model_name, prompt, user_stream = get_request_parameters(req.get_json())
+    model_to_use = map_model_name(model_name)
+    template_id = 2000000 if model_name in IMAGE_MODEL_NAMES else ''
+
+    if not messages and prompt:
+        final_user_content = prompt
+        first_user_message = final_user_content
+        image_url = None
+    elif messages:
+        last_message = messages[-1]
+        first_user_message, end_user_message, concatenated_messages = get_user_contents(messages, HISTORY_MSG_LIMIT)
+        final_user_content, image_url = process_content(last_message.get('content'))
+        final_user_content = concatenated_messages + '\n' + final_user_content if concatenated_messages else final_user_content
+
+    hash_value = generate_hash(first_user_message, model_to_use, token)
+    channel_id = get_channel_id(hash_value, token, model_to_use, final_user_content, template_id)
+
+    if final_user_content is None:
+        return Response("No user message found", status=400)
+
+    return send_chat_message(req, token, channel_id, final_user_content, model_to_use, user_stream, image_url,
+                             model_name)
+
+
+def handle_options_request():
+    return Response(status=204, headers={'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'})
+
+
+def generate_model_data():
+    return {
+        "object": "list",
+        "data": [{
+            "id": m,
+            "object": "model",
+            "created": int(datetime.now().timestamp()),
+            "owned_by": "popai"
+        } for m in IGNORED_MODEL_NAMES]
+    }
+
+
+def upload_file(file_path, token):
+    md5_hash = hashlib.md5()
+    with open(file_path, 'rb') as file:
+        # 分块读取文件内容，避免内存占用过大
+        for chunk in iter(lambda: file.read(4096), b""):
+            # 更新MD5哈希对象
+            md5_hash.update(chunk)
+    md5_value = md5_hash.hexdigest()
+    logging.info(f'md5_value : {md5_value}')
+    headers_step1 = {
+        'accept': 'application/json',
+        'accept-language': 'zh,en;q=0.9,zh-CN;q=0.8',
+        'app-name': 'popai-web',
+        'authorization': token,
+        'cache-control': 'no-cache',
+        'language': 'en',
+        'origin': 'https://www.popai.pro',
+        'pop-url': 'https://www.popai.pro/',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': 'https://www.popai.pro/',
+        'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    }
+
+    response_step1 = requests.get(f'https://api.popai.pro/py/api/v1/chat/getPresignedPost?md5={md5_value}',
+                                  headers=headers_step1)
+
+    if response_step1.status_code == 200:
+        presigned_data = response_step1.json()
+        print("Presigned data received:")
+        print(presigned_data)
+    else:
+        raise Exception(f"Failed to get presigned post: {response_step1.text}")
+    # 第二步: 使用预签名的 URL 上传文件
+    fields = response_step1.json()['data']['fields']
+    upload_url = presigned_data['data']['url']
+    # 构造上传文件的表单数据
+    form_data = {
+        'key': fields['key'],
+        'AWSAccessKeyId': fields['AWSAccessKeyId'],
+        'policy': fields['policy'],
+        'signature': fields['signature']
+    }
+
+    with open(file_path, 'rb') as file:
+        files = {'file': (file_path, file, 'application/pdf')}
+
+        headers_step2 = {
+            'Accept': 'application/json',
+            'Accept-Language': 'zh,en;q=0.9,zh-CN;q=0.8',
+            'App-Name': 'popai-web',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Origin': 'https://www.popai.pro',
+            'Pop-Url': 'https://www.popai.pro/',
+            'Pragma': 'no-cache',
+            'Referer': 'https://www.popai.pro/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+            'language': 'en',
+            'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"'
+        }
+
+        response_step2 = requests.post(upload_url, data=form_data, files=files, headers=headers_step2)
+
+    if response_step2.status_code == 204:
+        print("File uploaded successfully")
+    else:
+        raise Exception(f"Failed to upload file: {response_step2.text}")
+    # 第三步
+    url = 'https://api.popai.pro/py/api/v1/chat/create'
+
+    headers = {
+        'accept': 'application/json',
+        'accept-language': 'zh,en;q=0.9,zh-CN;q=0.8',
+        'app-name': 'popai-web',
+        'authorization': token,
+        'content-type': 'application/json',
+        'language': 'en',
+        'origin': 'https://www.popai.pro',
+        'pop-url': 'https://www.popai.pro/',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': 'https://www.popai.pro/',
+        'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+    }
+
+    data = {
+        "model": 'GPT-4',
+        "md5": md5_value,
+        # "fileName": file_path,
+        "fileContentType": 'application/pdf',
+        "extract_img_5k": True
+    }
+    logging.info(f'data: {data}')
+    logging.info(f'headers: {headers}')
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        print("Chat created successfully")
+        print(response.json())
+    else:
+        print(f"Failed to create chat: {response.status_code}")
+        print(response.text)
+
+
+# def upload_file(file_path):
+#     with open(file_path, 'rb') as file:
+#         response = requests.post('https://pixeldrain.com/api/file', files={'file': file})
+#         logging.info(response.text)
+#         if response.status_code == 201:
+#             response_data = response.json()
+#             file_id = response_data['id']
+#             return f"https://pixeldrain.com/api/file/{file_id}"
+#
+#         else:
+#             raise Exception(f"Failed to upload file: {response.json().get('message')}")
+
+
+def decode_base64_to_file(base64_data):
+    header, encoded = base64_data.split(',', 1)
+    # 根据MIME类型确定文件后缀
+    mime_type = header.split(';')[0].split(':')[1]
+    extension = {
+        'application/pdf': '.pdf',
+        'image/png': '.png',
+        'image/jpeg': '.jpg',
+        # 可以根据需要添加更多的 MIME 类型和扩展名
+    }.get(mime_type, '')
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=extension) as temp_file:
+        temp_file_path = temp_file.name
+
+    data = base64.b64decode(encoded)
+    with open(temp_file_path, 'wb') as file:
+        file.write(data)
+    return temp_file_path
+
+
+def is_base64(data):
+    try:
+        if data.startswith('data:'):
+            data = data.split(',', 1)[1]
+        base64.b64decode(data)
+        return True
+    except ValueError:
+        return False
+
+
+def is_url(data):
+    try:
+        result = urlparse(data)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
